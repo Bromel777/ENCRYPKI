60e552be4c3f3224338ec66e069f83a31146bfde
aleksandr <alexbromel@gmain.com>
2017-12-12 15:55:23 +0300

    AddedMempool

--- EncryApp.scala
+++ EncryApp.scala
@@ -9,9 +9,14 @@ import scorex.core.transaction.box.proposition.PublicKey25519Proposition
 import scorex.crypto.encode.Base16
 import encry.modifiers.history.EncryBlockHeader
 import encry.modifiers.mempool.EncryPaymentTransaction
+import encry.nodeView.mempool.EncryMempool
+import org.scalameter.utils.Tree
 import scorex.core.transaction.proof.Signature25519
 import scorex.crypto.authds.ADKey
 import scorex.crypto.signatures.{Curve25519, PrivateKey, PublicKey, Signature}
+
+import scala.collection.concurrent.TrieMap
+import scala.collection.mutable
 
 //class EncryApp(args: Seq[String]) extends Application {
 //  override type P = PublicKey25519Proposition
@@ -34,17 +39,30 @@ object EncryApp extends App{
   val pubKey : PublicKey = keyPair._2
   val priKey : PrivateKey = keyPair._1
   val prop = PublicKey25519Proposition(pubKey)
-  val sigNull = Signature25519(Curve25519.sign(priKey,"firstTransaction".getBytes))
+  val sigTX1 = Signature25519(Curve25519.sign(priKey,"firstTransaction".getBytes))
+  val sigTX2 = Signature25519(Curve25519.sign(priKey,"secondTransaction".getBytes))
 
-  val sigNullSeq : IndexedSeq[Signature25519] = IndexedSeq(sigNull)
+  val sigNullSeqTX1 : IndexedSeq[Signature25519] = IndexedSeq(sigTX1)
+  val sigNullSeqTX2 : IndexedSeq[Signature25519] = IndexedSeq(sigTX2)
 
   //val InputNull = IndexedSeq((PublicKey25519Proposition(PublicKey @@ Longs.toByteArray(123L)),12L))
   val InputNull = IndexedSeq((prop,12L))
-  val BaseTX = EncryPaymentTransaction(12,123L,OutputNull,sigNullSeq,InputNull)
+  val BaseTX1 = EncryPaymentTransaction(12,123L,OutputNull,sigNullSeqTX1,InputNull)
+  val BaseTX2 = EncryPaymentTransaction(28,106L,OutputNull,sigNullSeqTX2,InputNull)
 
-  for(a <- BaseTX.unlockers){
-    println(a.boxKey)
+//  for(a <- BaseTX.unlockers){
+//    println(a.boxKey)
+//  }
+
+  val txTree = List(BaseTX1,BaseTX2)
+  val tmf = EncryMempool.empty
+  tmf.put(txTree)
+  val txtr = tmf.unconfirmed
+  for((a,b) <- txtr){
+    println("in mempool " + b.fee)
   }
+
+//  println(new mutable.WrappedArray.ofByte(BaseTX2.hashNoNonces))
 
   def forceStopApplication(code: Int = 1): Unit =
     new Thread(() => System.exit(code), "encry-shutdown-thread").start()
